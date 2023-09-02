783cdc1b5fc8c665ad979d4cf5e3fd510a555178
Berat ARPA <Beratarpa@hotmail.com>
2023-09-02 08:48:37 +0300

    Add project files.

---
+++ WindowsFormsAppUI/ShellForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsAppUI
+{
+    partial class ShellForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShellForm));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.rdpClient = new AxMSTSCLib.AxMsRdpClient8NotSafeForScripting();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxYourIP = new System.Windows.Forms.TextBox();
+            this.textBoxClientIP = new System.Windows.Forms.TextBox();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rdpClient)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rdpClient, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConnect.Location = new System.Drawing.Point(620, 8);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 34);
+            this.buttonConnect.TabIndex = 3;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDisconnect.Enabled = false;
+            this.buttonDisconnect.Location = new System.Drawing.Point(701, 8);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 34);
+            this.buttonDisconnect.TabIndex = 4;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // rdpClient
+            // 
+            this.rdpClient.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rdpClient.Enabled = true;
+            this.rdpClient.Location = new System.Drawing.Point(0, 50);
+            this.rdpClient.Margin = new System.Windows.Forms.Padding(0);
+            this.rdpClient.Name = "rdpClient";
+            this.rdpClient.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdpClient.OcxState")));
+            this.rdpClient.Size = new System.Drawing.Size(784, 511);
+            this.rdpClient.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Controls.Add(this.textBoxUserName);
+            this.panel1.Controls.Add(this.textBoxClientIP);
+            this.panel1.Controls.Add(this.textBoxYourIP);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonDisconnect);
+            this.panel1.Controls.Add(this.buttonConnect);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(784, 50);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Client IP";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(424, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Your IP";
+            // 
+            // textBoxYourIP
+            // 
+            this.textBoxYourIP.Location = new System.Drawing.Point(8, 25);
+            this.textBoxYourIP.Name = "textBoxYourIP";
+            this.textBoxYourIP.ReadOnly = true;
+            this.textBoxYourIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxYourIP.TabIndex = 100;
+            // 
+            // textBoxClientIP
+            // 
+            this.textBoxClientIP.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxClientIP.Location = new System.Drawing.Point(215, 25);
+            this.textBoxClientIP.Name = "textBoxClientIP";
+            this.textBoxClientIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxClientIP.TabIndex = 0;
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxUserName.Location = new System.Drawing.Point(321, 25);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUserName.TabIndex = 1;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxPassword.Location = new System.Drawing.Point(427, 25);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // ShellForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "ShellForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shell";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.rdpClient)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private AxMSTSCLib.AxMsRdpClient8NotSafeForScripting rdpClient;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxClientIP;
+        private System.Windows.Forms.TextBox textBoxYourIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ WindowsFormsAppUI/ShellForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppUI
+{
+    public partial class ShellForm : Form
+    {
+        public ShellForm()
+        {
+            InitializeComponent();
+            textBoxYourIP.Text = GetMyIP();
+        }
+
+        private async void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxClientIP.Text) || string.IsNullOrEmpty(textBoxUserName.Text) || string.IsNullOrEmpty(textBoxPassword.Text))
+            {
+                MessageBox.Show("Enter the required information!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            rdpClient.Server = textBoxClientIP.Text;
+            rdpClient.UserName = textBoxUserName.Text;
+            rdpClient.AdvancedSettings2.ClearTextPassword = textBoxPassword.Text;
+            rdpClient.AdvancedSettings8.EnableCredSspSupport = true;
+
+            //Optional
+            rdpClient.ColorDepth = 32;
+            rdpClient.DesktopWidth = rdpClient.Width;
+            rdpClient.DesktopHeight = rdpClient.Height;
+            rdpClient.AdvancedSettings3.SmartSizing = true;
+
+            rdpClient.Connect();
+
+            await Task.Delay(2000);
+
+            if (rdpClient.Connected.ToString() == "1")
+            {
+                buttonConnect.Enabled = false;
+                buttonDisconnect.Enabled = true;
+            }
+            else
+            {
+                buttonConnect.Enabled = true;
+                buttonDisconnect.Enabled = false;
+            }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (rdpClient.Connected.ToString() == "1")
+            {
+                rdpClient.Disconnect();
+                buttonConnect.Enabled = true;
+                buttonDisconnect.Enabled = false;
+            }
+        }
+
+        public string GetMyIP()
+        {
+            var hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (var host in hostEntry.AddressList)
+            {
+                if (host.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return host.ToString();
+                }
+            }
+
+            return "0.0.0.0";
+        }
+    }
+}
